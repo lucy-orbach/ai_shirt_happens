4494219b62c98b3ec7bb12aae5edf777c467ced4
Steven Nunez <steven.nunez@gmail.com>
2015-05-06 10:16:04 -0400

    Seed new shirts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505145959) do
+ActiveRecord::Schema.define(version: 20150506135940) do
 
   create_table "labels", force: :cascade do |t|
     t.integer  "shirt_id"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20150505145959) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "artist"
+    t.string   "image_url"
   end
 
   create_table "tags", force: :cascade do |t|
