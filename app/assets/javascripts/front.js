fbd880b6e7d079a07c618aea69ee05cb0893570f
lucy-orbach <lucy.orbach@gmail.com>
2015-05-23 16:49:33 -0400

    its working

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,25 +1,41 @@
 $(document).ready(function($) {
   $('.full-container').height($(window).height());
-  $('main').height($(window).height());
-  $('.banner.skewed').delay(1200).queue(function(next) {
-  	$(this).removeClass('skewed');
-  	next();
-	});
-  
+  $('main').height($(window).height()); 
 });
 
 $(function () {
-    $("#showMe").click(function (event) {
-       // $("main").slideDown(1000);
-        $('.gallery-container').removeClass('slide-out');
-    });
+  $("#showMe").click(function (event) {
+     // $("main").slideDown(1000);
+      $('.gallery-container').removeClass('slide-out');
+      $('main').removeClass('fcontainer');
+      $('main').height('auto');
+  });
 });
  
-   
-// $(function () {
-//   $("#showMe").click(function() {
-//     //$('main').slideDown();
-//     $('main').removeClass('hidden');
-//   });
+$(function initialAnimation () {
+  $('.banner.skewed').delay(1200).queue(function(next) {
+    $(this).removeClass('skewed');
+    $('.fade-out').toggleClass('fade-out fade-in');
+    next();
+  });
+}); 
 
-// });
+
+$(function(){
+  $("#addCollection").click(function(){  
+    debugger;
+   $.ajax({
+      url: $(this).attr('href'),
+      method: $(this).attr('data-method'),
+      dataType: 'script',
+      context: this
+    }).done(function(){
+        debugger;
+        $('#recommendations').removeClass('fade-out');
+        $('main').removeClass('fcontainer');
+      });done
+    return false;
+  });
+});//function
+
+
--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -3,8 +3,13 @@ class OwnershipsController < ApplicationController
     shirt = Shirt.find(params[:shirt_id])
     current_user.shirts << shirt
     current_user.like(shirt)
-    redirect_to shirt, notice: "#{shirt.name} has been added to your collection"
-  rescue ActiveRecord::RecordInvalid => e
-    redirect_to shirt, notice: e.message
+
+    respond_to do |format|
+      format.html { redirect_to shirt}
+      format.js { }
+    end
+   #  redirect_to shirt, notice: "#{shirt.name} has been added to your collection"
+  	# rescue ActiveRecord::RecordInvalid => e
+   #  redirect_to shirt, notice: e.message
   end
 end
